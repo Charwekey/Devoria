727314a8d5b01ca5dacc71e5df6eac551e97f2fa
Charwekey <crabisabutey@gmail.com>
2026-05-17 23:21:52 +0000

    Update CORS policy to allow Vercel frontend requests

--- backend/utils/main.py
+++ backend/utils/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # CORS - allow frontend to communicate
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
